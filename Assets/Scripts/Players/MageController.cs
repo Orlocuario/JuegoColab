91377da1cb91397b782e92a8b2e662309fa3dc37
Jaime Y <jsyanez@uc.cl>
2017-07-18 14:23:29 -0400

    Fixed animations

--- Assets/Scripts/Players/MageController.cs
+++ Assets/Scripts/Players/MageController.cs
@@ -3,4 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MageController : PlayerController {
+
+    protected override void Update()
+    {
+        base.Update();
+    }
 }
