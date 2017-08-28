587a3d4a259b15926873c1769a42da48501ad251
Orlocuario <orlocuario@gmail.com>
2017-08-27 19:09:07 -1100

    switch

--- Assets/Scripts/Ambiente/Interruptores/SwitchActions.cs
+++ Assets/Scripts/Ambiente/Interruptores/SwitchActions.cs
@@ -16,30 +16,36 @@ public class SwitchActions : MonoBehaviour {
     }
 
 	public void DoSomething()
-    {
-        switch(groupId)
-        {
-			case 3789:
-				Animator animadorRoca = GameObject.FindGameObjectWithTag ("rocaql").GetComponent<Animator> ();
-				animadorRoca.SetBool ("rocasecae",true);
-                break;
-            case 1:
-                //asdada
-                break;
-			case 3:
-				GameObject platescalera = (GameObject)Instantiate(Resources.Load("Prefabs/Plat0.6"));
-				platescalera.GetComponent<Transform>().position = new Vector2 (43f, -17f);
-				GameObject feedbackSwitch = (GameObject)Instantiate (Resources.Load ("Prefabs/));
+	{
+		switch (groupId) {
+		case 3789:
+			Animator animadorRoca = GameObject.FindGameObjectWithTag ("rocaql").GetComponent<Animator> ();
+			animadorRoca.SetBool ("rocasecae", true);
+			break;
 
-					break;
-	            default:
-	                break;
+		case 0:
+			GameObject platescalerain = (GameObject)Instantiate (Resources.Load ("Prefabs/MagePlath"));
+			platescalerain.GetComponent<Transform> ().position = new Vector2 (-3.67f, -4.2f);
+			GameObject feedbackswitchin = (GameObject)Instantiate (Resources.Load ("Prefabs/FeedbackParticles/FBMageButt"));
+			feedbackswitchin.GetComponent<Transform> ().position = new Vector2 (8.224f, -0.976f);
+			break;
 
-			/*GameObject tevoyamataroe = GameObject.FindGameObjectWithTag ("plataforma1");
-			GameObject.Destroy (tevoyamataroe);
+		case 1:
+            
+			break;
+		case 2:
+			GameObject platescalera = (GameObject)Instantiate (Resources.Load ("Prefabs/MagePlath"));
+			platescalera.GetComponent<Transform> ().position = new Vector2 (43f, -16f);
+			GameObject feedbackswitch = (GameObject)Instantiate (Resources.Load ("Prefabs/FeedbackParticles/FBMageButt"));
+			feedbackswitch.GetComponent<Transform> ().position = new Vector2 (41.4f, -16.3f);
+			break;
+		default:
+			break;
 
-			GameObject fireball = (GameObject)Instantiate (Resources.Load ("Prefabs/Attacks/BolaM1"));
-			fireball.GetComponent<Transform> ().position = new Vector2 (3, 4);*/
-        }
-    }
-}
+		//GameObject tevoyamataroe = GameObject.FindGameObjectWithTag ("plataforma1");
+		//GameObject.Destroy (tevoyamataroe);
+		//GameObject fireball = (GameObject)Instantiate (Resources.Load ("Prefabs/Attacks/BolaM1"));
+		//fireball.GetComponent<Transform> ().position = new Vector2 (3, 4);*/
+			}
+		}
+	}
