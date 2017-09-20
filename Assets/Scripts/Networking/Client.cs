1ba2fbc55fe0037317cd4e617e6b5b69d2534eba
Jaime Y <jsyanez@uc.cl>
2017-09-20 00:24:40 -0300

    Agregado soporte de mensajes largos. Falta testearlo

--- Assets/Scripts/Networking/Client.cs
+++ Assets/Scripts/Networking/Client.cs
@@ -15,6 +15,7 @@ public class Client : MonoBehaviour {
     int port = 7777;
     int socketId; // Host ID
     int connectionId;
+    int bigChannelId;
     int channelId;
     public static Client instance;
     int bufferSize = 100;
@@ -26,6 +27,7 @@ public class Client : MonoBehaviour {
         NetworkTransport.Init();
         ConnectionConfig config = new ConnectionConfig();
         channelId = config.AddChannel(QosType.Unreliable);
+        bigChannelId = config.AddChannel(QosType.ReliableFragmented);
         HostTopology topology = new HostTopology(config, 10);
         socketId = NetworkTransport.AddHost(topology, port);
         handler = new ClientMessageHandler();
@@ -48,6 +50,17 @@ public class Client : MonoBehaviour {
         NetworkTransport.Send(socketId, connectionId, channelId, buffer, bufferSize, out error);
     }
 
+    public void SendMessageToPlanner(string message)
+    {
+        byte error;
+        //int bytes = System.Text.ASCIIEncoding.ASCII.GetByteCount(message);
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, message);
+        NetworkTransport.Send(socketId, connectionId, bigChannelId, buffer, 1024, out error);
+    }
+
     void LateUpdate()
     {
         int recSocketId;
@@ -57,7 +70,13 @@ public class Client : MonoBehaviour {
         int dataSize;
         byte error;
         NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recSocketId, out recConnectionId, out recChannelId, recBuffer, bufferSize, out dataSize, out error);
-
+        NetworkError Error = (NetworkError)error;
+        if (Error == NetworkError.MessageToLong)
+        {
+            //Trata de capturar el mensaje denuevo, pero asumiendo buffer más grande.
+            recBuffer = new byte[1024];
+            recNetworkEvent = NetworkTransport.Receive(out recSocketId, out recConnectionId, out recChannelId, recBuffer, 1024, out dataSize, out error);
+        }
         switch (recNetworkEvent)
         {
             case NetworkEventType.Nothing:
@@ -69,13 +88,25 @@ public class Client : MonoBehaviour {
                 Stream stream = new MemoryStream(recBuffer);
                 BinaryFormatter formatter = new BinaryFormatter();
                 string message = formatter.Deserialize(stream) as string;
+                if(recChannelId == channelId)
+                {
+                    handler.HandleMessage(message);
+                }
+                if(recChannelId == bigChannelId)
+                {
+                    ReceiveMessageFromPlanner(message, recConnectionId);
+                }
                 Debug.Log("incoming message event received: " + message);
-                handler.HandleMessage(message);
                 break;
             case NetworkEventType.DisconnectEvent:
                 Debug.Log("disconnected from server");
                 break;
         }
+    }
+
+    private void ReceiveMessageFromPlanner(string message, int connectionId)
+    {
+        //Algo
     }
 
     public PlayerController GetPlayerController(int charId)
