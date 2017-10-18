9f47b6499b0decf5ce7488758915f8e0607adf46
li0t <leonardo0ramos@gmail.com>
2017-10-18 13:45:09 -0300

    Clean Display HUD code

--- Assets/Scripts/Networking/DisplayHUD.cs
+++ Assets/Scripts/Networking/DisplayHUD.cs
@@ -22,28 +22,32 @@ public class DisplayHUD: MonoBehaviour{
         hpCurrentPercentage = message;
 
         Text percentageText = GameObject.Find("HealthPercentage").GetComponent<Text>();
-        if (float.Parse(hpCurrentPercentage) <= 0.2f)
+        float hpCurrentPercentageFloat = float.Parse(hpCurrentPercentage);
+
+
+        if (hpCurrentPercentageFloat <= 0.2f)
         {
-            percentageText.text = "<color=#e67f84ff>" + (float.Parse(hpCurrentPercentage) * 100).ToString("0") + "%" + "</color>";
+            percentageText.text = "<color=#e67f84ff>" + (hpCurrentPercentageFloat * 100).ToString("0") + "%" + "</color>";
             GameObject.Find("CurrentHealth").GetComponent<Image>().color = new Color32(174, 0, 0, 255);
             GameObject.Find("HealthMask").GetComponent<Image>().color = new Color32(77, 0, 0, 255);
         }
-        else if (float.Parse(hpCurrentPercentage) <= 0.5f)
+        else if (hpCurrentPercentageFloat <= 0.5f)
         {
-            percentageText.text = "<color=#f9ca45ff>" + (float.Parse(hpCurrentPercentage) * 100).ToString("0") + "%" + "</color>";
+            percentageText.text = "<color=#f9ca45ff>" + (hpCurrentPercentageFloat * 100).ToString("0") + "%" + "</color>";
             GameObject.Find("CurrentHealth").GetComponent<Image>().color = new Color32(174, 174, 0, 190);
             GameObject.Find("HealthMask").GetComponent<Image>().color = new Color32(77, 77, 0, 255);
         }
         else
         {
-            percentageText.text = "<color=#64b78eff>" + (float.Parse(hpCurrentPercentage) * 100).ToString("0") + "%" + "</color>";
+            percentageText.text = "<color=#64b78eff>" + (hpCurrentPercentageFloat * 100).ToString("0") + "%" + "</color>";
             GameObject.Find("CurrentHealth").GetComponent<Image>().color = new Color32(0, 135, 0, 255);
             GameObject.Find("HealthMask").GetComponent<Image>().color = new Color32(0, 77, 0, 255);
         }
         
         float maxLimitWidth = GameObject.Find("HealthMask").GetComponent<RectTransform>().sizeDelta.x;
         float currentY = GameObject.Find("HealthMask").GetComponent<RectTransform>().sizeDelta.y;
-        float currentX = float.Parse(hpCurrentPercentage) * maxLimitWidth;        
+        float currentX = hpCurrentPercentageFloat * maxLimitWidth;      
+        
         GameObject.Find("CurrentHealth").GetComponent<RectTransform>().sizeDelta = new Vector2(currentX, currentY);
     }
 
@@ -52,24 +56,28 @@ public class DisplayHUD: MonoBehaviour{
         mpCurrentPercentage = message;
 
         Text percentageText = GameObject.Find("ManaPercentage").GetComponent<Text>();
-        percentageText.text = (float.Parse(mpCurrentPercentage) * 100).ToString("0") + "%";
+        float mpCurrentPercentageFloat = float.Parse(mpCurrentPercentage);
+
+        percentageText.text = (mpCurrentPercentageFloat * 100).ToString("0") + "%";
         
         float maxLimitWidth = GameObject.Find("ManaMask").GetComponent<RectTransform>().sizeDelta.x;
         float currentY = GameObject.Find("ManaMask").GetComponent<RectTransform>().sizeDelta.y;
-        float currentX = float.Parse(mpCurrentPercentage) * maxLimitWidth;
+        float currentX = mpCurrentPercentageFloat * maxLimitWidth;
+
         GameObject.Find("CurrentMana").GetComponent<RectTransform>().sizeDelta = new Vector2(currentX, currentY);
     }
 
     public void ExperienceBar(string message)
     {
         expCurrentPercentage = message;
+        float expCurrentPercentageFloat = float.Parse(expCurrentPercentage);
 
         Text percentageText = GameObject.Find("ExpPercentage").GetComponent<Text>();
-        percentageText.text = "Exp: " + (float.Parse(expCurrentPercentage) * 100).ToString("0") + "%";
+        percentageText.text = "Exp: " + (expCurrentPercentageFloat * 100).ToString("0") + "%";
 
         float maxLimitWidth = GameObject.Find("ExpMask").GetComponent<RectTransform>().sizeDelta.x;
         float currentY = GameObject.Find("ExpMask").GetComponent<RectTransform>().sizeDelta.y;
-        float currentX = float.Parse(expCurrentPercentage) * maxLimitWidth;
+        float currentX = expCurrentPercentageFloat * maxLimitWidth;
         GameObject.Find("CurrentExp").GetComponent<RectTransform>().sizeDelta = new Vector2(currentX, currentY);
     }
 }
