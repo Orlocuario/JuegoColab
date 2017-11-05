b64ce5a164b5ac79909c1062dee264ee3b0dc1ef
li0t <leonardo0ramos@gmail.com>
2017-11-05 13:56:58 -0300

    Moving switches

---
+++ Assets/Scripts/Ambiente/MovingObjects/SwitchMovingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMovingObject : MovingObject
+{
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        IgnoreSwitchMovingObjects();
+    }
+
+    private void IgnoreSwitchMovingObjects()
+    {
+        GameObject[] movingSwitchs = GameObject.FindGameObjectsWithTag("SwitchMovingObject");
+        Collider2D collider = GetComponent<Collider2D>();
+
+        foreach (GameObject movingSwitch in movingSwitchs)
+        {
+            Physics2D.IgnoreCollision(collider, movingSwitch.GetComponent<Collider2D>());
+        }
+    }
+
+}
