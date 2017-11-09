140d7b925bc38d5d632fd2d832839977b5dc16ec
li0t <leonardo0ramos@gmail.com>
2017-11-09 12:45:29 -0300

    PlayerBlocker

---
+++ Assets/Scripts/Scene/Ambient/Objects/Killing/PlayerBlocker.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * When a player touches this objects it loses health and respwans,
+ * allows for some players to pass
+ */
+public class PlayerBlocker : KillingObject
+{
+    #region Attributes
+
+    public PlayerController[] playersCanPass;
+
+    #endregion
+
+    #region Events
+
+    // Attack those who enter the alert zone
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (GameObjectIsPlayer(other.gameObject))
+        {
+            if (!GameObjectCanPass(other.gameObject))
+            {
+                Kill();
+            }
+        }
+    }
+
+    #endregion
+
+    #region Utils
+
+    protected bool GameObjectCanPass(GameObject other)
+    {
+        bool passes = false;
+
+        foreach (PlayerController canPass in playersCanPass)
+        {
+            if (other.GetComponent<PlayerController>() == canPass)
+            {
+                passes = true;
+                break;
+            }
+        }
+
+        return passes;
+    }
+
+    #endregion
+
+}
+
+
