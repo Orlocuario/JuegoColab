bcf7c9e08789dbb8860bda7ef4794ca6877083cc
li0t <leonardo0ramos@gmail.com>
2017-11-16 12:47:53 -0300

    Faster regeneration

--- Assets/Scripts/Scene/UI/Chat/ChatZone.cs
+++ Assets/Scripts/Scene/UI/Chat/ChatZone.cs
@@ -12,7 +12,7 @@ public class ChatZone : MonoBehaviour
     public GameObject chatButtonOn;
     
     private static string regenerationUnits = "1";
-    private static int regenerationFrameRate = 30;
+    private static int regenerationFrameRate = 25;
     private static float activationDistance = 2f;
 
     private int regenerationFrame;
